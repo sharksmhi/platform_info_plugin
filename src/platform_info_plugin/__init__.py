59fc044e5c7f249f7d2c50aecd94c3f0c2962eed
a001985 <magnus.wenzer@smhi.se>
2025-03-05 09:28:30 +0100

    Add platform info funktion

--- src/platform_info_plugin/__init__.py
+++ src/platform_info_plugin/__init__.py
@@ -1,18 +1,22 @@
 import pathlib
 
-import uuid
-
 from . import exceptions
 
 
-def get_platform_info(
+def get_platform_info(**kwargs) -> dict:
+    return dict(
+        platform_name='Platformen',
+    )
+
+
+def get_current_data(
         path_to_credentials: pathlib.Path | str = None,
         # credentials: dict[str, str] = None,
         **kwargs,
         ) -> dict[str, str | int | float]:
     return dict(
-        event_id=str(uuid.uuid4()),
-        parent_event_id=str(uuid.uuid4()),
+        # event_id=str(uuid.uuid4()),
+        # parent_event_id=str(uuid.uuid4()),
         cruise_nr='10',
         latitude='5757.4',   # or lat
         longitude='1212.2',  # or lon
@@ -24,5 +28,9 @@ def get_platform_info(
         station='Teststation',
         ship_name='Svea',
         ship_code='77SE',
+        air_pres='',
+        air_temp='',
+        wind_spd='',
+        wind_dir='',
     )
 
